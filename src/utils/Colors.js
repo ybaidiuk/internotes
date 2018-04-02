333a420ccce6a4dd235adbf03b0f48fa08a79459
Baidiuk Yevhen <y.baidiuk@gmail.com>
2018-04-02 13:28:55 +0200

    add color schema

---
+++ src/utils/Colors.js
@@ -0,0 +1,10 @@
+export default colors = {
+  black: '#18222D',
+  dark: '#213040',
+  barkBlue: '#3D6A97',
+  gray: '#A8B9CA',
+  lightBlue: '#2DA5FD',
+  yellow: '#FFD427',
+  red: '#E8584E',
+  green: '#44C355',
+};
