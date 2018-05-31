d90c9f1aa314d83a6dd098ce3abb27b1aa7407e4
Baidiuk Yevhen <y.baidiuk@gmail.com>
2018-05-31 23:21:37 +0200

    add encrypt and decrypt methods to CryptoUtils

--- __tests__/CryptoUtils.js
+++ __tests__/CryptoUtils.js
@@ -1,5 +1,30 @@
 import CryptoUtils from '../src/utils/CryptoUtils';
 
-it('CryptoUtils', () => {
+it('CryptoUtilsSha256Test', () => {
   expect(CryptoUtils.sha256(CryptoUtils.sha256('qwerty'))).toBe(CryptoUtils.sha256x2('qwerty'));
+  expect(CryptoUtils.sha256('qwerty')).toEqual(expect.not.stringMatching(CryptoUtils.sha256('qweRty')));
+});
+
+it('CryptoUtilsEncryptTextTest', () => {
+  const text = "Hello World ,\n  i'm love Bitcoin";
+  console.log(text);
+  const privateKey = CryptoUtils.sha256("LiteCoin");
+
+  const encryptedText = CryptoUtils.encrypt(text, privateKey);
+  console.log(encryptedText);
+
+  const message = CryptoUtils.decrypt(encryptedText, privateKey);
+  console.log(message);
+
+  //check decryption
+  expect(text).toBe(message);
+
+
+  const encryptedText2 = CryptoUtils.encrypt(text, privateKey);
+  const message2 = CryptoUtils.decrypt(encryptedText2, privateKey);
+  console.log(encryptedText);
+  console.log(encryptedText2);
+  console.log(message);
+  console.log(message2);
+  expect(message).toBe(message2);
 });
--- src/utils/CryptoUtils.js
+++ src/utils/CryptoUtils.js
@@ -1,4 +1,5 @@
 import SHA256 from 'crypto-js/sha256';
+import CryptoJS from 'crypto-js';
 
 export default class CryptoUtils {
   //tested
@@ -9,5 +10,15 @@ export default class CryptoUtils {
   //tested
   static sha256x2(massage) {
     return this.sha256(this.sha256(massage));
+  }
+
+  //tested
+  static encrypt(text, key) {
+    return CryptoJS.AES.encrypt(text, key).toString();
+  }
+//tested
+  static decrypt(cipherText, key) {
+    let bytes = CryptoJS.AES.decrypt(cipherText, key);
+    return bytes.toString(CryptoJS.enc.Utf8);
   }
 }
